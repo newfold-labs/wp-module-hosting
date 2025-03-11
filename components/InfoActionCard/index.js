b5ee4411d2cd9ca93d20fbb3b8e1b4c8acb8c392
arunshenoy99 <devarunshenoy99@gmail.com>
2025-03-11 08:20:28 +0530

    Add React Support and Basic Components

---
+++ components/InfoActionCard/index.js
@@ -0,0 +1,98 @@
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { Card } from '@newfold/ui-component-library';
+import { InformationCircleIcon } from '@heroicons/react/24/solid';
+
+const InfoActionCard = ( { title, infoText, items } ) => {
+	const [ copiedIndex, setCopiedIndex ] = useState( null );
+
+	const handleAction = ( value, index, actionType, actionUrl ) => {
+		if ( actionType === 'copy' ) {
+			window.navigator.clipboard.writeText( value );
+			setCopiedIndex( index );
+
+			setTimeout( () => {
+				setCopiedIndex( null );
+			}, 2000 );
+		} else if ( actionType === 'redirect' ) {
+			window.open( actionUrl, '_blank', 'noopener,noreferrer' );
+		}
+	};
+
+	return (
+		<Card className="nfd-p-6 nfd-border nfd-border-gray-200 nfd-rounded-lg">
+			<div className="nfd-flex nfd-items-center nfd-space-x-2">
+				<h3 className="nfd-text-lg nfd-font-medium">{ title }</h3>
+				{ infoText && (
+					<InformationCircleIcon
+						className="nfd-h-4 nfd-w-4 nfd-text-gray-500"
+						title={ infoText }
+					/>
+				) }
+			</div>
+
+			<div className="nfd-mt-3">
+				{ items.map(
+					(
+						{
+							label,
+							value,
+							actionText,
+							infoText: itemInfotext,
+							actionType,
+							actionUrl,
+						},
+						index
+					) => (
+						<div
+							key={ index }
+							className="nfd-relative nfd-flex nfd-justify-between nfd-items-center nfd-mb-2"
+						>
+							<div className="nfd-flex nfd-items-center nfd-space-x-2">
+								{ label && (
+									<p className="nfd-font-bold">{ label }</p>
+								) }
+								{ itemInfotext && (
+									<InformationCircleIcon
+										className="nfd-h-4 nfd-w-4 nfd-text-gray-500"
+										title={ itemInfotext }
+									/>
+								) }
+								<p className="nfd-text-gray-700">{ value }</p>
+							</div>
+							{ actionText && (
+								<div className="nfd-relative">
+									<button
+										className="nfd-text-[#0E3E80] nfd-font-bold nfd-text-sm nfd-no-underline"
+										onClick={ () =>
+											handleAction(
+												value,
+												index,
+												actionType,
+												actionUrl
+											)
+										}
+									>
+										{ actionText }
+									</button>
+									{ actionType === 'copy' &&
+										copiedIndex === index && (
+											<div className="nfd-absolute nfd-bottom-full nfd-left-1/2 nfd-transform -nfd-translate-x-1/2 nfd-mb-2 nfd-bg-[#0E3E80] nfd-text-white nfd-text-xs nfd-font-semibold nfd-py-1 nfd-px-2 nfd-rounded-md nfd-shadow-md">
+												{ __(
+													'Copied!',
+													'wp-module-hosting'
+												) }
+												<div className="nfd-absolute nfd-left-1/2 nfd-transform -nfd-translate-x-1/2 nfd-w-0 nfd-h-0 nfd-border-l-4 nfd-border-l-transparent nfd-border-r-4 nfd-border-r-transparent nfd-border-t-4 nfd-border-t-[#0E3E80] nfd-top-full"></div>
+											</div>
+										) }
+								</div>
+							) }
+						</div>
+					)
+				) }
+			</div>
+		</Card>
+	);
+};
+
+export default InfoActionCard;
