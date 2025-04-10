dc73f38232ac8a6517324112a118850c635ef0a3
arunshenoy99 <devarunshenoy99@gmail.com>
2025-04-10 13:23:38 +0530

    Cypress: Fix Refresh Data Test

--- tests/cypress/integration/refreshdata.cy.js
+++ tests/cypress/integration/refreshdata.cy.js
@@ -1,14 +1,51 @@
 import { cleanupVisitHomePage, wpLogin } from '../support/utils';
-import { spyOnHostingPanelApi } from '../support/intercepts';
+import {
+	interceptPanelAndReplaceKey,
+	spyOnHostingPanelApi,
+} from '../support/intercepts';
 import { testEnabledForPlugin } from '../support/utils';
 
 describe( 'Refreshing Data', () => {
-	before( function () {
+	before( () => {
 		testEnabledForPlugin( 'bluehost' );
 		wpLogin();
+
+		const fiveMinutesAgo = Math.floor( Date.now() / 1000 ) - 5 * 60;
+
+		interceptPanelAndReplaceKey( 'object-cache', {}, ( body ) => {
+			body.__generated = fiveMinutesAgo;
+			if ( body.__meta ) {
+				body.__meta.generated = fiveMinutesAgo;
+			}
+		} );
+
 		cy.visit( '/wp-admin/admin.php?page=bluehost#/hosting' );
-		spyOnHostingPanelApi();
+		cy.wait( '@getPanelData' ); // First call is overridden
+	} );
+
+	it( 'shows initial refresh time and updates it on refresh click', () => {
+		cy.get( '[data-testid="nfd-data-refresh-time"]' )
+			.should( 'exist' )
+			.and( 'not.be.empty' )
+			.invoke( 'text' )
+			.as( 'initialTime' );
+
+		cy.get( '[data-testid="nfd-data-refresh-button"]' )
+			.should( 'exist' )
+			.and( 'not.be.disabled' )
+			.as( 'refreshButton' );
+
+		spyOnHostingPanelApi(); // Reattach before click
+		cy.get( '@refreshButton' ).click();
 		cy.wait( '@getPanelData' );
+
+		cy.get( '@initialTime' ).then( ( initialText ) => {
+			cy.get( '[data-testid="nfd-data-refresh-time"]' )
+				.should( 'exist' )
+				.and( 'not.be.empty' )
+				.invoke( 'text' )
+				.should( 'not.equal', initialText );
+		} );
 	} );
 
 	it('Check refresh time text and Refresh button click', () => {
