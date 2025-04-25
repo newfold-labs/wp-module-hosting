50d96f3acd782ea04f8671a70194b2e22339e4db
Evan Mullins <evanrm@gmail.com>
2025-04-25 14:51:22 -0400

    update changed string

--- build/hosting/hosting.min.asset.php
+++ build/hosting/hosting.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '84276463a045d2d0f616');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'cb26be75561201eb9ec1');
