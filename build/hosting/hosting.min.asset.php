f2c90e1c256d96a62051efc4f786707d80f773cb
Evan Mullins <evanrm@gmail.com>
2025-04-25 14:42:35 -0400

    update to use build files in i18n script

--- build/hosting/hosting.min.asset.php
+++ build/hosting/hosting.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '5bf3e0f355bccfd90e8f');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '84276463a045d2d0f616');
--- src/sections/Panel/getPanelText.js
+++ src/sections/Panel/getPanelText.js
@@ -3,13 +3,21 @@ import { __ } from '@wordpress/i18n';
 const getPanelText = () => {
 	return {
 		loading: __(
-			'Hang tight, we\'re gathering the latest info…',
+			"Hang tight, we're gathering the latest info…",
 			'wp-module-hosting'
 		),
 		error: __( 'Error:', 'wp-module-hosting' ),
 		title: __( 'Hosting', 'wp-module-hosting' ),
-		descriptionWithPlan: __( 'A general overview of your %s hosting plan and your site\'s performance.', 'wp-module-hosting' ),
-		description: __( 'A general overview of your hosting and your site\'s performance.', 'wp-module-hosting' ),
+		descriptionWithPlan:
+			/* translators: %s: hosting plan name */
+			__(
+				"A general overview of your %s hosting plan and your site's performance.",
+				'wp-module-hosting'
+			),
+		description: __(
+			"A general overview of your hosting and your site's performance.",
+			'wp-module-hosting'
+		),
 		planNameFallback: __( 'PLAN_NAME', 'wp-module-hosting' ),
 		manageButton: __( 'Manage Hosting', 'wp-module-hosting' ),
 	};
