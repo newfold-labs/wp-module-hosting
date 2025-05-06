5fda3ea32ff6c16104f23c73589ae8042928d305
Alessio Torrisi <alessiotorrisidev@gmail.com>
2025-05-06 16:25:13 +0200

    Tweak: use API to get PHP version

--- includes/PHPVersion/PHPVersion.php
+++ includes/PHPVersion/PHPVersion.php
@@ -13,16 +13,39 @@ class PHPVersion {
 	 * @return array PHP version details.
 	 */
 	public function get_data() {
-		$current_version     = phpversion();
-		$recommended_version = '8.3';
+        $site_id = 'xxxx';
+        $token = 'xx';
+
+		$response = wp_remote_get(
+			"https://hosting.uapi.newfold.com/v1/sites/{$site_id}/settings/php-versions",
+			array(
+				'headers' => array(
+				    'Authorization' => 'Bearer ' . $token,
+                )
+			)
+		);
+
+		if ( ! is_wp_error( $response ) ) {
+			$body = wp_remote_retrieve_body( $response );
+			$api_data = json_decode( $body, true );
+
+			if ( isset( $api_data['latest'] ) ) {
+				$current_version = $api_data['latest'];
+			}
+
+			if ( isset( $api_data['default'] ) ) {
+				$recommended_version = $api_data['default'];
+			}
+		}else{
+            $current_version     = phpversion();
+            $recommended_version = '8.3';
+        }
 
 		$data = array(
 			'current_version' => $current_version,
+            'recommended_version' => $recommended_version
 		);
 
-		if ( version_compare( $current_version, $recommended_version, '<' ) ) {
-			$data['recommended_version'] = $recommended_version;
-		}
 
 		return $data;
 	}
