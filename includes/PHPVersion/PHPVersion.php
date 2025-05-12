225a21aac9883340687cc1299db4ac67bdce0a09
Alessio Torrisi <alessiotorrisidev@gmail.com>
2025-05-12 15:28:32 +0200

    New: use HUAPI to retrieve PHP data

--- includes/PHPVersion/PHPVersion.php
+++ includes/PHPVersion/PHPVersion.php
@@ -2,10 +2,19 @@
 
 namespace NewfoldLabs\WP\Module\Hosting\PHPVersion;
 
+use NewfoldLabs\WP\Module\Hosting\Helpers\HUAPIHelper;
+
 /**
  * Handles PHP version-related functionality.
  */
 class PHPVersion {
+
+	/**
+	 * API endpoint for name servers.
+	 *
+	 * @var string
+	 */
+	protected $huapi_endpoint = '/v1/sites/site_id/settings/php-versions';
 
 	/**
 	 * Retrieves PHP version data.
@@ -13,39 +22,36 @@ class PHPVersion {
 	 * @return array PHP version details.
 	 */
 	public function get_data() {
-        $site_id = 'xxxx';
-        $token = 'xx';
 
-		$response = wp_remote_get(
-			"https://hosting.uapi.newfold.com/v1/sites/{$site_id}/settings/php-versions",
-			array(
-				'headers' => array(
-				    'Authorization' => 'Bearer ' . $token,
-                )
-			)
-		);
+		$current_version     = phpversion();
+		$recommended_version = '8.3';
 
-		if ( ! is_wp_error( $response ) ) {
-			$body = wp_remote_retrieve_body( $response );
-			$api_data = json_decode( $body, true );
+		$site_id = HUAPIHelper::get_site_id();
 
-			if ( isset( $api_data['latest'] ) ) {
-				$current_version = $api_data['latest'];
+		if ( ! is_wp_error( $site_id ) ) {
+
+			$endpoint = str_replace( 'site_id', $site_id, $this->huapi_endpoint );
+			$helper   = new HUAPIHelper( $endpoint, array(), 'GET' );
+			$response = $helper->send_request();
+
+			if ( ! is_wp_error( $response ) ) {
+				$body     = wp_remote_retrieve_body( $response );
+				$api_data = json_decode( $body, true );
+
+				if ( isset( $api_data['latest'] ) ) {
+					$current_version = $api_data['latest'];
+				}
+
+				if ( isset( $api_data['default'] ) ) {
+					$recommended_version = $api_data['default'];
+				}
 			}
-
-			if ( isset( $api_data['default'] ) ) {
-				$recommended_version = $api_data['default'];
-			}
-		}else{
-            $current_version     = phpversion();
-            $recommended_version = '8.3';
-        }
+		}
 
 		$data = array(
-			'current_version' => $current_version,
-            'recommended_version' => $recommended_version
+			'current_version'     => $current_version,
+			'recommended_version' => $recommended_version,
 		);
-
 
 		return $data;
 	}
