9d8f63b43bb60705694e6cee4c4f6b7cfe66a7ef
Armando Liccardo <li.armando82@gmail.com>
2025-05-19 15:19:44 +0100

    adding BH logo to hosting page

--- build/hosting/hosting.min.asset.php
+++ build/hosting/hosting.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'cb57bb04f3b4abe1a0bf');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'ff7e99b375f938a2c061');
