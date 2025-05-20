8463f7ed78a584b34cb56ec24fbb8194fea8e82b
Armando Liccardo <li.armando82@gmail.com>
2025-05-20 12:36:19 +0100

    fix js error

--- build/hosting/hosting.min.asset.php
+++ build/hosting/hosting.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '47bf2bd59f6c8ea764a7');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '20de3499a5da5b9925e1');
