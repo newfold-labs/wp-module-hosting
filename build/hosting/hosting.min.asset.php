8d83a3b36dfd9f5c4276fae5d8e7ba186776d14f
Armando Liccardo <li.armando82@gmail.com>
2025-06-04 13:36:33 +0100

    adding compiled js

--- build/hosting/hosting.min.asset.php
+++ build/hosting/hosting.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0396a8f827f3f211b07f');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f0cc04aa620b354fc36e');
