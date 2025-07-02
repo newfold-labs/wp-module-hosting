83dbbbe665037e02bfd22b14225db89d05cab8a0
Alessio Torrisi <alessiotorrisidev@gmail.com>
2025-07-02 09:33:10 +0200

    Merge conflicts

--- build/hosting/hosting.min.asset.php
+++ build/hosting/hosting.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f0cc04aa620b354fc36e');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9c63f76f3ba89e57e6ba');
