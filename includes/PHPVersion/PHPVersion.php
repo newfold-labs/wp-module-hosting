a4e7ae12a3693871b0c948f79f045f4e63715fdc
Alessio Torrisi <alessiotorrisidev@gmail.com>
2025-07-08 14:34:43 +0200

    Fix: SSH Info and PHP Version sections

--- includes/PHPVersion/PHPVersion.php
+++ includes/PHPVersion/PHPVersion.php
@@ -2,28 +2,56 @@
 
 namespace NewfoldLabs\WP\Module\Hosting\PHPVersion;
 
+use NewfoldLabs\WP\Module\Hosting\Helpers\HUAPIHelper;
+
 /**
  * Handles PHP version-related functionality.
  */
 class PHPVersion {
 
-	/**
-	 * Retrieves PHP version data.
-	 *
-	 * @return array PHP version details.
-	 */
-	public function get_data() {
-		$current_version     = phpversion();
-		$recommended_version = '8.3';
+    /**
+     * API endpoint for name servers.
+     *
+     * @var string
+     */
+    protected $huapi_endpoint = '/v1/sites/site_id/settings/php-versions';
 
-		$data = array(
-			'current_version' => $current_version,
-		);
+    /**
+     * Retrieves PHP version data.
+     *
+     * @return array PHP version details.
+     */
+    public function get_data() {
 
-		if ( version_compare( $current_version, $recommended_version, '<' ) ) {
-			$data['recommended_version'] = $recommended_version;
-		}
+        $current_version     = phpversion();
+        $recommended_version = '8.3';
 
-		return $data;
-	}
+        $site_id = HUAPIHelper::get_site_id();
+
+        if ( ! is_wp_error( $site_id ) ) {
+
+            $endpoint = str_replace( 'site_id', $site_id, $this->huapi_endpoint );
+            $helper   = new HUAPIHelper( $endpoint, array(), 'GET' );
+            $response = $helper->send_request();
+
+            if ( ! is_wp_error( $response ) ) {
+                $api_data = json_decode( $response, true );
+
+                if ( isset( $api_data['latest'] ) ) {
+                    $current_version = $api_data['latest'];
+                }
+
+                if ( isset( $api_data['default'] ) ) {
+                    $recommended_version = $api_data['default'];
+                }
+            }
+        }
+
+        $data = array(
+            'current_version'     => $current_version,
+            'recommended_version' => $recommended_version,
+        );
+
+        return $data;
+    }
 }
--- includes/SSHInfo/SSHInfo.php
+++ includes/SSHInfo/SSHInfo.php
@@ -4,75 +4,115 @@ namespace NewfoldLabs\WP\Module\Hosting\SSHInfo;
 
 use NewfoldLabs\WP\Module\Hosting\Helpers\PlatformHelper;
 
+use NewfoldLabs\WP\Module\Hosting\Helpers\HUAPIHelper;
+
 /**
  * Handles SSH login information retrieval.
  */
 class SSHInfo {
 
-	/**
-	 * Dependency container instance.
-	 *
-	 * @var mixed
-	 */
-	protected $container;
+    /**
+     * Dependency container instance.
+     *
+     * @var mixed
+     */
+    protected $container;
 
-	/**
-	 * SSHInfo constructor.
-	 *
-	 * @param mixed $container The dependency container instance.
-	 */
-	public function __construct( $container ) {
-		$this->container = $container;
-	}
 
-	/**
-	 * Retrieves SSH login info using the real server hostname and filesystem username.
-	 *
-	 * @return array SSH login data.
-	 */
-	public function get_data() {
-		// Use the helper to check if the platform is 'atomic'
-		if ( PlatformHelper::is_atomic() ) {
-			return array(
-				'ssh_info' => '',
-			);
-		}
+    /**
+     * API endpoint for SSH Info.
+     *
+     * @var string
+     */
+    protected $huapi_shared_endpoint = array(
+        'shared' => '/v1/hosting/hosting_id/ssh',
+        'cloud'  => '/v2/sites/site_id/ssh-users',
+    );
 
-		$ip       = $this->get_host_ip_from_hostname();
-		$username = $this->get_server_username();
-		$ssh_info = $username && $ip ? "{$username}@{$ip}" : '';
+    /**
+     * SSHInfo constructor.
+     *
+     * @param mixed $container The dependency container instance.
+     */
+    public function __construct( $container ) {
+        $this->container = $container;
+    }
 
-		return array(
-			'ssh_info' => $ssh_info,
-		);
-	}
+    /**
+     * Retrieves SSH login info using the real server hostname and filesystem username.
+     *
+     * @return array SSH login data.
+     */
+    public function get_data() {
 
-	/**
-	 * Retrieves the IP address based on the server's hostname.
-	 *
-	 * @return string|null IP address or null if not found.
-	 */
-	private function get_host_ip_from_hostname() {
-		$hostname = gethostname();
-		$ip       = gethostbyname( $hostname );
+        $customer_id = HUAPIHelper::get_customer_id();
 
-		// Fallback in case resolution fails or returns the hostname itself
-		if ( empty( $ip ) || $ip === $hostname ) {
-			return null;
-		}
+        $site_id = HUAPIHelper::get_site_id();
 
-		return $ip;
-	}
+        $is_atomic = PlatformHelper::is_atomic();
 
-	/**
-	 * Retrieves the server username from the WordPress installation path.
-	 *
-	 * @return string|null Server username or null if not found.
-	 */
-	private function get_server_username() {
-		$absolute_path = ABSPATH;
-		$parts         = explode( '/', trim( $absolute_path, '/' ) );
+        if ( ( $is_atomic && is_wp_error( $customer_id ) ) || is_wp_error( $site_id ) ) {
+            return null;
+        }
 
-		return isset( $parts[1] ) ? $parts[1] : null;
-	}
+        $huapi_endpoint = PlatformHelper::is_atomic() ? $this->huapi_shared_endpoint['cloud'] : $this->huapi_shared_endpoint['shared'];
+
+        $endpoint = PlatformHelper::is_atomic() ? str_replace( 'site_id', $site_id, $huapi_endpoint ) : str_replace( 'hosting_id', $customer_id, $huapi_endpoint );
+
+        $helper   = new HUAPIHelper( $endpoint, array(), 'GET' );
+        $response = $helper->send_request();
+
+        if ( is_wp_error( $response ) ) {
+            // error_log( 'Error in the API call: ' . $response->get_error_message() ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+            return null;
+        }
+
+        $data = json_decode( $response, true );
+
+        if ( $is_atomic ) {
+            if ( isset( $data['users'] ) && is_array( $data['users'] ) && count( $data['users'] ) > 0 ) {
+                $ssh_info = $data['users'][0]['user'] . '@' . $data['users'][0]['ssh_hostname'];
+            } else {
+                $ssh_info = '';
+            }
+        } elseif ( isset( $data['credential'] ) ) {
+            $ssh_info = $data['credential'];
+        } else {
+            $ip       = $data['ip'] ?? $this->get_host_ip_from_hostname();
+            $username = $this->get_server_username();
+            $ssh_info = $username && $ip ? "{$username}@{$ip}" : '';
+        }
+
+        return array(
+            'ssh_info' => $ssh_info,
+        );
+    }
+
+    /**
+     * Retrieves the IP address based on the server's hostname.
+     *
+     * @return string|null IP address or null if not found.
+     */
+    private function get_host_ip_from_hostname() {
+        $hostname = gethostname();
+        $ip       = gethostbyname( $hostname );
+
+        if ( empty( $ip ) || $ip === $hostname ) {
+            return null;
+        }
+
+        return $ip;
+    }
+
+    /**
+     * Retrieves the server username from the WordPress installation path.
+     *
+     * @return string|null Server username or null if not found.
+     */
+    private function get_server_username() {
+        $absolute_path = ABSPATH;
+        $parts         = explode( '/', trim( $absolute_path, '/' ) );
+
+        return isset( $parts[1] ) ? $parts[1] : null;
+    }
 }
