bf7d7f8e2884a547a00533afee99ee102165a390
Armando Liccardo <li.armando82@gmail.com>
2025-07-09 14:27:55 +0100

    added cypress test

---
+++ tests/cypress/integration/flush-transient.cy.js
@@ -0,0 +1,49 @@
+import {
+	deleteTransient,
+	getTransient,
+} from '../support/serverHelpers';
+import constants from '../fixtures/constants.json';
+import { wpLogin, testEnabledForPlugin } from '../support/utils';
+import { spyOnHostingPanelApi } from '../support/intercepts';
+
+const PANEL_TRANSIENT = constants.transients.hostingPanelData;
+
+describe( 'Flush Hosting Panel cache transient on Permalink structure change', () => {
+	before( () => {
+		cy.exec(
+			`npx wp-env run cli wp rewrite structure '/%postname%/'`	
+		);
+		testEnabledForPlugin( 'bluehost' );
+		wpLogin();
+		cy.visit( '/wp-admin/admin.php?page=nfd-hosting' );
+		spyOnHostingPanelApi();
+		cy.wait( '@getPanelData' );
+	}) ;
+
+	it( 'Change Permalinks Structure and check if transient is deleted', () => {
+		getTransient( PANEL_TRANSIENT ).then( ( value ) => {
+    		expect(value).to.not.be.empty;
+		});
+
+		/* Change the permalink structure */
+		cy.exec(
+			`npx wp-env run cli wp rewrite structure '/%year%/' --hard`	
+		).then( ({ stdout }) => {
+			/* Check transient has been removed */
+			getTransient( PANEL_TRANSIENT ).then( ( value ) => {
+				expect(value).to.be.empty;
+			}).then( () => {
+				cy.reload().then( () => {
+					/* reload the hosting panel page and check a new transient has been saved */
+					getTransient( PANEL_TRANSIENT ).then( ( value ) => {
+						expect(value).to.not.be.empty;
+					});
+
+					cy.exec(
+						`npx wp-env run cli wp rewrite structure '/%postname%/'`	
+					);
+				});
+			});
+		});
+	} );
+} );
