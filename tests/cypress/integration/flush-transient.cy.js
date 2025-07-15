b1243cf2478e64867f938d44ebbc957ea7a5457f
Armando Liccardo <li.armando82@gmail.com>
2025-07-15 08:57:36 +0100

    added a cleanup action to reset the permalink structure

--- tests/cypress/integration/flush-transient.cy.js
+++ tests/cypress/integration/flush-transient.cy.js
@@ -9,15 +9,18 @@ const PANEL_TRANSIENT = constants.transients.hostingPanelData;
 
 describe( 'Flush Hosting Panel cache transient on Permalink structure change', () => {
 	before( () => {
-		cy.exec(
-			`npx wp-env run cli wp rewrite structure '/%postname%/'`	
-		);
 		testEnabledForPlugin( 'bluehost' );
 		wpLogin();
 		cy.visit( '/wp-admin/admin.php?page=nfd-hosting' );
 		spyOnHostingPanelApi();
 		cy.wait( '@getPanelData' );
-	}) ;
+	});
+
+	after( () => {
+		cy.exec(
+			`npx wp-env run cli wp rewrite structure '/%postname%/'`	
+		);
+	});
 
 	it( 'Change Permalinks Structure and check if transient is deleted', () => {
 		getTransient( PANEL_TRANSIENT ).then( ( value ) => {
@@ -37,10 +40,6 @@ describe( 'Flush Hosting Panel cache transient on Permalink structure change', (
 					getTransient( PANEL_TRANSIENT ).then( ( value ) => {
 						expect(value).to.not.be.empty;
 					});
-
-					cy.exec(
-						`npx wp-env run cli wp rewrite structure '/%postname%/'`	
-					);
 				});
 			});
 		});
