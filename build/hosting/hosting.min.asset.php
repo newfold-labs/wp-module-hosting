c9210991a38705d973dfcb34b915eaeee1975bd3
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-07-28 11:34:38 +0100

    Include wp-module-link-tracker module

--- build/hosting/hosting.min.asset.php
+++ build/hosting/hosting.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '6737dd718b9d439f0cd2');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'd72605b5b0c683660e50');
